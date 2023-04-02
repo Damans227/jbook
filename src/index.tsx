bfd3ba1a4f72829e27d4f4eb6ca14e0217a31084
Damans227 <damans227@gmail.com>
2023-04-02 18:31:54 -0400

    add esbuild module

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,39 @@
-import ReactDOM  from "react-dom";
-import { useState } from "react";
+import ReactDOM from "react-dom";
+import { useState, useEffect, useRef } from "react";
+import * as esbuild from 'esbuild-wasm'
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugins";
 
 const App = () => {
     const [input, setInput] = useState('')
     const [code, setCode] = useState('')
+    const ref = useRef<any>()
 
-    const onClick = () => {
-        console.log({input})
+    const startService = async () => {
+        ref.current  = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        })
+    }
+
+    useEffect(() => {
+        startService()
+    }, [])
+
+    const onClick = async () => {
+        if(!ref.current){
+            return
+        }
+
+    const result = await ref.current.build({
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false, 
+        plugins: [unpkgPathPlugin()]
+    })
+
+    console.log({result})
+
+        setCode(result.outputFiles[0].text)
     }
 
     return <div>
@@ -19,5 +46,5 @@ const App = () => {
 }
 
 ReactDOM.render(
-    <App/>, document.querySelector('#root')
+    <App />, document.querySelector('#root')
 )
